1718bb74ebff3ca810be113b572d96f27b53e6a3
Filip Dabrowski <filip.a.dabrowski@gmail.com>
2025-05-22 10:18:18 +0200

    Refactored get_billboard_data.py
    Adjusted the script so it fits to the current structure

--- data/billboard/get_billboard_data.py
+++ data/billboard/get_billboard_data.py
@@ -3,7 +3,7 @@ import pandas as pd
 import os
 
 # Define path to kaggle JSON
-os.environ["KAGGLE_CONFIG_DIR"] = os.path.abspath("../config")
+os.environ["KAGGLE_CONFIG_DIR"] = os.path.abspath("../../config")
 
 # Get data from kaggle - dataset containing top 100 songs each year from 1958
 path = kagglehub.dataset_download("dhruvildave/billboard-the-hot-100-songs")
@@ -14,6 +14,6 @@ csv_path = os.path.join(path, "charts.csv")
 df = pd.read_csv(csv_path)
 
 # Save CSV
-output_path = "../data/billboard_data_2015_2024.csv"
+output_path = "billboard_data_2015_2024.csv"
 df.to_csv(output_path, index=False)
 print("Billboard data saved to:", output_path)
