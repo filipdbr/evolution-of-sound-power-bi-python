b8a39c84a3cfaf4f8a4d5c68c5f7a8b58b033b72
Filip Dabrowski <filip.a.dabrowski@gmail.com>
2025-05-22 09:46:44 +0200

    Created script and downloaded the billboard top 100 dataset

---
+++ data/get_billboard_data.py
@@ -0,0 +1,19 @@
+import kagglehub
+import pandas as pd
+import os
+
+# Define path to kaggle JSON
+os.environ["KAGGLE_CONFIG_DIR"] = os.path.abspath("../config")
+
+# Get data from kaggle - dataset containing top 100 songs each year from 1958
+path = kagglehub.dataset_download("dhruvildave/billboard-the-hot-100-songs")
+print("Dataset downloaded to:", path)
+
+# Read CSV
+csv_path = os.path.join(path, "charts.csv")
+df = pd.read_csv(csv_path)
+
+# Save CSV
+output_path = "../data/billboard_data_2015_2024.csv"
+df.to_csv(output_path, index=False)
+print("Billboard data saved to:", output_path)
